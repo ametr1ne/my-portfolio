9c5a5f4b90e2cae973251be919420ec9aadc5b59
ametr1ne <nikon.filanovsky@gmail.com>
2023-03-03 01:05:47 +0300

    adding gsap lib

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,73 +1,72 @@
-/*--- Preloader -----*/
+// gsap animations
 
-window.onload = () => {
-    document.querySelector('.preloader').classList.add('hidden')
-}
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+let smoother = ScrollSmoother.create({
+    smooth: 1.5,  
+    effects: true
+  });
+
 
 /*---- Burger -----*/
 
-const burger = document.querySelector('.header__burger')
+const burger = document.querySelector(".header__burger");
 
-burger.addEventListener('click', function () {
-    document.querySelector('.header__menu').classList.toggle('open')
+burger.addEventListener("click", function () {
+  document.querySelector(".header__menu").classList.toggle("open");
 
-    if (window.matchMedia("(max-width: 767px)").matches) {
-        document.body.classList.toggle('overflow-hidden')
-    }
-})
-
+  if (window.matchMedia("(max-width: 767px)").matches) {
+    document.body.classList.toggle("overflow-hidden");
+  }
+});
 
 if (window.matchMedia("(max-width: 767px)").matches) {
+  const header_links = document.querySelectorAll(".header__link");
 
-    const header_links = document.querySelectorAll('.header__link');
-
-    header_links.forEach(function (value) {
-        value.addEventListener('click', function () {
-            document.body.classList.remove('overflow-hidden')
-            document.querySelector('.header__menu').classList.remove('open')
-        })
-    })
+  header_links.forEach(function (value) {
+    value.addEventListener("click", function () {
+      document.body.classList.remove("overflow-hidden");
+      document.querySelector(".header__menu").classList.remove("open");
+    });
+  });
 }
 
 /*------- portfolio is empty -------*/
 
 /*-------- interactive greeting -------*/
 
-let date = new Date().getHours()
-let greeting = ''
+let date = new Date().getHours();
+let greeting = "";
 
 switch (true) {
-    case date >= 23 || date < 5:
-        greeting = 'night'
-        break
-    case 5 <= date && date < 12:
-        greeting = 'morning'
-        break
-    case 12 <= date && date < 16:
-        greeting = 'afternoon'
-        break
-    case 16 <= date && date < 23:
-        greeting = 'evening'
-        break
+  case date >= 23 || date < 5:
+    greeting = "night";
+    break;
+  case 5 <= date && date < 12:
+    greeting = "morning";
+    break;
+  case 12 <= date && date < 16:
+    greeting = "afternoon";
+    break;
+  case 16 <= date && date < 23:
+    greeting = "evening";
+    break;
 }
 
-document.querySelector('.interactive').innerHTML = greeting
+document.querySelector(".interactive").innerHTML = greeting;
 
 /*---- smooth scroll ------*/
 
-const anchors = document.querySelectorAll('a[href*="#"]')
+const anchors = document.querySelectorAll('a[href*="#"]');
 
 for (let anchor of anchors) {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault()
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-        const blockID = anchor.getAttribute('href').substr(1)
+    const blockID = anchor.getAttribute("href").substr(1);
 
-        document.getElementById(blockID).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        })
-    })
+    smoother.scrollTo("#" + blockID, true, "top top");
+  });
 }
 
 /*-------------------- FOR TESTS --------------------------*/
